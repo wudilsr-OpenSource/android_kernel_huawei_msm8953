886f2f0ecfbb2dacd8ccf8f24685048f2e165e08
stic-server-open <1138705738@qq.com>
2023-07-17 15:58:38 +0800

    Subject: [PATCH] [DEBUG] qpnp-power-on: Press the power button to force a  kernel panic
    * This submission is only for debug use, such as GSI ROM,
      we may get some useful logs generated at startup.
    
    * This patch change needs to be merged and submitted for use
      similar to "forced hot restart", because RAM cannot save data
      after power failure, so it is necessary to force hot restart
      to make RAM restart to REC(TWRP) to obtain logs.
    
    * If the GSI ROM is stuck on the first boot screen (LOGO), press
      and hold the power button to force restart until rec can not get
      any logs, this commit changes the purpose, if stuck on the first
      boot screen (LOGO), press the power button to trigger the
      kernel panic completes the hot restart and leaves dmesg logs,
      try to get some useful help, such as init, selinux and other reasons.
    
    Change-Id: I615dadd24eaa13cae09034d40756c94deba64aa4

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -964,6 +964,11 @@ qpnp_get_cfg(struct qpnp_pon *pon, u32 pon_type)
 	return NULL;
 }
 
+static void panic_outdmesg(void)
+{
+	panic("A panic hot restart has been triggered");
+}
+
 static int
 qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 {
@@ -1039,6 +1044,9 @@ qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 		input_report_key(pon->pon_input, cfg->key_code, 1);
 		input_sync(pon->pon_input);
 	}
+
+	if (cfg->pon_type == PON_KPDPWR && key_status)
+		panic_outdmesg();
 
 	input_report_key(pon->pon_input, cfg->key_code, key_status);
 	input_sync(pon->pon_input);
